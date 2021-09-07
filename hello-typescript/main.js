3939acf7387c51e51ce35b0a187610a9a8df1d61
Igor <igor.ptz@hotmail.com>
2021-09-07 19:34:53 -0300

    First commit

---
+++ hello-typescript/main.js
@@ -0,0 +1,17 @@
+var minhaVariavel = 'minha variavel';
+function myFunction(x, y) {
+    return x + y;
+}
+var num = 2;
+var num2 = 2;
+var numbers = [1, 2, 3];
+numbers.map(function (value) { return value * 2; });
+var Soma = /** @class */ (function () {
+    function Soma() {
+    }
+    Soma.prototype.somar = function (x, y) {
+        return x + y;
+    };
+    return Soma;
+}());
+var string1 = 'KKKKKK';
---
+++ hello-typescript/main.ts
@@ -0,0 +1,24 @@
+var minhaVariavel = 'minha variavel';
+
+function myFunction(x, y) {
+
+    return x + y;
+}
+
+let num = 2;
+
+const num2 = 2;
+
+let numbers = [1, 2, 3];
+
+numbers.map(value => value * 2);
+
+class Soma {
+
+    somar(x, y) {
+
+        return x + y;
+    }
+}
+
+var string1 : string = 'KKKKKK';
